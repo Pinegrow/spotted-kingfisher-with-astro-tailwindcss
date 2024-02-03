be7b808f839e6fae31fd221fc2fe3bc318fec890
Akayy <techakayy@gmail.com>
2024-02-03 22:25:35 +1100

    Updated unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import svelte from '@astrojs/svelte'
 import Pinegrow from '@pinegrow/astro-module'
 import AutoImportComponents from 'unplugin-vue-components/vite'
 import AutoImportAPIs from 'unplugin-auto-import/astro'
-import Unocss from 'unocss/astro'
+import Unocss from 'unocss/vite'
 import presetIcons from '@unocss/preset-icons'
 // import VueDevTools from 'vite-plugin-vue-devtools'
 // import myAstroModule from './src/modules/my-module'
@@ -49,18 +49,6 @@ export default defineConfig({
       include: ['**/solid/*'],
     }),
     svelte(),
-    Unocss({
-      presets: [
-        presetIcons({
-          prefix: 'i-', // default prefix, do not change
-        }),
-      ],
-      content: {
-        pipeline: {
-          include: ['./src/**/*'],
-        },
-      },
-    }),
     mdx(),
     sitemap(),
     // For details, refer to https://github.com/antfu/unplugin-auto-import#configuration
@@ -125,6 +113,19 @@ export default defineConfig({
         include: [/\.vue$/, /\.vue\?vue/, /\.md$/, /\.mdx?/],
         // resolvers: [], // Auto-import using resolvers
         dts: 'components.d.ts',
+      }),
+      Unocss({
+        presets: [
+          presetIcons({
+            prefix: 'i-', // default prefix, do not change
+          }),
+        ],
+        content: {
+          pipeline: {
+            /* Please ensure that you update the filenames and paths to accurately match those used in your project. */
+            include: ['./src/**/*'],
+          },
+        },
       }),
       // VueDevTools()
     ],
