85d1021b6288b681d0e08323cd3a6194e4003bf2
Akayy <techakayy@gmail.com>
2024-12-05 22:48:36 +1100

    Refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,6 @@ import AutoImportComponents from 'unplugin-vue-components/vite'
 import AutoImportAPIs from 'unplugin-auto-import/astro'
 import Unocss from 'unocss/vite'
 import presetIcons from '@unocss/preset-icons'
-import VueDevTools from 'vite-plugin-vue-devtools'
 // import myAstroModule from './src/modules/my-module'
 
 // import { visualizer } from 'rollup-plugin-visualizer'
@@ -130,9 +129,6 @@ export default defineConfig({
             include: ['./src/**/*'],
           },
         },
-      }),
-      VueDevTools({
-        appendTo: 'app.ts',
       }),
     ],
 
